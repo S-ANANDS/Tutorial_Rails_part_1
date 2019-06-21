dba2eb6cc619d5d801645308c81c1c8b039da06c
Anand <anandnair96@gmail.com>
2019-06-21 18:26:44 +0530

    created a static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
-  root 'users#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  'static_pages/help'
+  get  'static_pages/about'
+  root 'static_pages#home'
 end
